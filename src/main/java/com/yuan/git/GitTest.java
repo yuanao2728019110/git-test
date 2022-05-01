85801cda538559915cda331b5fea1692fac78afa
yuan <2728019110@qq.com>
2022-05-01 21:52:37 +0800

    first commit

---
+++ src/main/java/com/yuan/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yuan.git;
+
+/*
+ *@author:ya
+ *@date:2022/5/1 21:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
